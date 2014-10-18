06a9027b6af2a13653baa67b7ab39a028b1b36b7
Jon Haddad <jon@jonhaddad.com>
2014-10-18 13:20:16 -0700

    adding pypi and versioning

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+#next time:
+#python setup.py register
+#python setup.py sdist upload
+
+version = open('ironeagle/VERSION', 'r').readline().strip()
+
+long_desc = """
+
+"""
+
+setup(
+    name='ironeagle',
+    version=version,
+    description='Helper library for working with Pandas and the Cassandra Native Driver',
+    long_description=long_desc,
+    classifiers = [
+        "Environment :: Web Environment",
+        "Environment :: Plugins",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='cassandra,cql,pandas',
+    install_requires = ['cassandra-driver >= 2.1.0', 'pandas >= 1.7.2'],
+    author='Jon Haddad',
+    author_email='jon@jonhaddad.com',
+    url='https://github.com/rustyrazorblade/ironeagle',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+)
